b85d717cd7e3a626439fa0dd9935b70638bb3e0d
HP Probook 640 <HP Probook 640@DESKTOP-HCJ90MD>
2024-02-22 22:18:35 +0500

    Add project files.

---
+++ Project60/Animals.cpp
@@ -0,0 +1,13 @@
+
+#include "Animals.h"
+int main() {
+    Lion lion;
+    Elephant elephant;
+    Monkey monkey;
+    cout << "Lion: " << lion.makeSound() << endl;
+    cout << "Elephant: " << elephant.makeSound() << endl;
+    cout << "Monkey: " << monkey.makeSound() << endl;
+
+
+    return 0;
+}
